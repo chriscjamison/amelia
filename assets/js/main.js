ec27a81312896f84064d4ad738339d9b3a54a131
C.C. Jamison <chris@chriscjamison.com>
2015-07-13 17:10:28 -0500

    new file:   assets/css/main.css 	new file:   assets/img/land/1280/jpg/main-1.jpg 	new file:   assets/img/land/1280/jpg/main-2.jpg 	new file:   assets/img/land/1280/jpg/main-3.jpg 	new file:   assets/img/land/1280/jpg/main-4.jpg 	new file:   assets/img/land/1280x800.jpg 	new file:   assets/img/land/1600/jpg/main-1.jpg 	new file:   assets/img/land/1600/jpg/main-2.jpg 	new file:   assets/img/land/1600/jpg/main-3.jpg 	new file:   assets/img/land/1600/jpg/main-4.jpg 	new file:   assets/img/land/1600/png/main-1.png 	new file:   assets/img/land/1600/png/main-2.png 	new file:   assets/img/land/1600/png/main-3.png 	new file:   assets/img/land/1600/png/main-4.png 	new file:   assets/img/land/1600/png/main-a-1.png 	new file:   assets/img/land/1600/png/main-a-2.png 	new file:   assets/img/land/1600/png/main-a-3.png 	new file:   assets/img/land/1600/png/main-a-4.png 	new file:   assets/img/logo/Thumbs.db 	new file:   assets/img/logo/logo_main.png 	new file:   assets/img/logo/logo_sub.png 	new file:   assets/img/logo/logo_with_text_lg.png 	new file:   assets/img/menu/default.png 	new file:   assets/img/spcr/Thumbs.db 	new file:   assets/img/spcr/landing_pg_16x16.png 	new file:   assets/img/spcr/landing_pg_234x37.png 	new file:   assets/img/spcr/landing_pg_237x24.png 	new file:   assets/js/jquery-2.1.4.js 	new file:   assets/js/main.js 	new file:   sc/index.htm

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+
+
+var windowWidth = new Number;
+var windowHeight = new Number;
+var divHeight = new Number;
+var divValue = new Number;
+
+var backgroundDim = new String;
+
+var divValues = new Object;
+
+$(document).ready( 
+		function ()	{
+			windowWidth = ($(document).width());
+			windowHeight = ($(document).height());
+			
+			console.log("windowWidth = " + windowWidth);
+			console.log("windowHeight = " + windowHeight);
+			
+			divHeight = windowHeight / 4;
+			console.log("windowHeight / 4 = " + Math.round(windowHeight / 4));
+			
+			divValues = {
+				width: windowWidth,
+				height: divHeight//,
+				backgroundSize: "contain"
+			}
+			
+			//backgroundDim = windowWidth + "px";
+			$("#land DIV").css(divValues);
+			
+			console.log(divValues);
+		}
+);
+
