8aa6c0e9e38c306a912d087bc4776efd18276d9c
Chris Jamison <halcyon.systems@chriscjamison.com>
2015-11-17 19:22:06 -0800

    0.6b - Basic parallax framework is complete. Need to adjust CSS to fit HTML flush with window.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,7 +4,7 @@ function loadQuestion(questionData_Array) {
 			url: questionData_Array[0],
 			statusCode: {
 				404: function () {
-					$(questionData_Array[1]).html("<a href=\"/amelia/sc/sctn/1/index.htm\">Please click here to restart the quiz</a>");
+					$(questionData_Array[1]).html("<a href=\"javascript:loadQuiz('initial')\">Please click here to restart the quiz</a>");
 				}
 			}
 		}
@@ -18,7 +18,7 @@ function loadQuestion(questionData_Array) {
 						url: questionData_Array[2],
 						statusCode: {
 							404: function () {
-								$(questionData_Array[3]).html("<a href=\"/amelia/sc/sctn/1/index.htm\">Please click here to restart the quiz</a>");
+								$(questionData_Array[3]).html("<a href=\"javascript:loadQuiz('initial')\">Please click here to restart the quiz</a>");
 							}
 						}
 					}
@@ -30,16 +30,47 @@ function loadQuestion(questionData_Array) {
 				);
 			}
 	);
-	
+}
+
+function loadInfo(infoData_Array) {	
+	$.ajax(
+		{
+			url: infoData_Array[0],
+			statusCode: {
+				404: function () {
+					$(infoData_Array[1]).html("<a href=\"javascript:loadContactForm('initial')\">Please click here to restart the quiz</a>");
+				}
+			}
+		}
+	).done(
+			function (html) {
+				$(infoData_Array[1]).empty();
+				$(infoData_Array[1]).append(html);
+				
+				$.ajax(
+					{
+						url: infoData_Array[2],
+						statusCode: {
+							404: function () {
+								$(infoData_Array[3]).html("<a href=\"javascript:loadContactForm('initial')\">Please click here to restart the quiz</a>");
+							}
+						}
+					}
+				).done(
+						function (html) {
+							$(infoData_Array[3]).empty();
+							$(infoData_Array[3]).append(html);
+						}
+				);
+			}
+	);
 }
 
 function loadQuiz(questionSet) {
-	var i;
 	var questionNum;
 
 	var questionPath = "/amelia/assets/ajax/quiz/qstn_";
 	var questionSuffix = ".htm";
-	var questionFilename;
 	
 	var columnDivElement_Array = new Array();
 
@@ -69,4 +100,40 @@ function loadQuiz(questionSet) {
 	questionData_Array[3] = columnDivElement_Array[1];
 	
 	loadQuestion(questionData_Array);
+}
+
+function loadContactForm(infoSet) {
+	var infoNum;
+
+	var infoPath = "/amelia/assets/ajax/cntct/info_";
+	var infoSuffix = ".htm";
+	
+	var columnDivElement_Array = new Array();
+
+	var infoData_Array = new Array();
+
+	switch (infoSet) {
+		case "initial":
+			infoNum = 1;
+		break;
+
+		case "set_1":
+			infoNum = 3;
+		break;
+	} // END OF switch statement
+	
+	columnDivElement_Array = [
+		"#cntct-clmn-1",
+		"#cntct-clmn-2"
+	];
+	
+	infoData_Array[0] = infoPath + infoNum + infoSuffix;
+	
+	infoData_Array[1] = columnDivElement_Array[0];
+	
+	infoData_Array[2] = infoPath + (infoNum + 1) + infoSuffix;
+		
+	infoData_Array[3] = columnDivElement_Array[1];
+	
+	loadInfo(infoData_Array);
 }
