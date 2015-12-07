98e28f4c076b7f6043a4d2bafc0a6a0b9dc806c5
Chris Jamison <halcyon.systems@chriscjamison.com>
2015-12-06 23:12:13 -0800

    0.7.1.1 - Section animations do not trigger properly

--- assets/js/animtn.js
+++ assets/js/animtn.js
@@ -1,17 +1,35 @@
+function fadeSectionBlocks(sectionBlockElements, transitionTimeValue) {
+	$(sectionBlockElements).each(
+		function () {
+			$(this).fadeTo(transitionTimeValue, 1);
+		}
+	);
+}
+
 function animateElements(sectionNum) {
 	if (sectionNum !== "main")	{
 		var headerClassString = "div.headr.sctn_" + sectionNum;
 		var sectionDescElementsString = "div.sctn_" + sectionNum + "-blok";
+		var transitionValue = 500;
 	
-		$(headerClassString).fadeTo(250, 1, 
-			function () {
-				$(sectionDescElementsString).each(
-					function () {
-						$(this).fadeTo(250, 1);
-					}
-				);
-			}	
-		);
+		fadeSectionBlocks(headerClassString, transitionValue);
+		
+		switch (sectionNum) {
+			case 1:
+				fadeSectionBlocks(sectionDescElementsString, transitionValue);
+			break;
+			case 2:
+				var sectionDescElementsString_2 = "div.sctn_2-blok, div.sctn_2-blok-desc, div.sctn_2-blok_2";
+				fadeSectionBlocks(sectionDescElementsString_2, transitionValue);
+			break;
+			case 3:
+				var sectionDescELementsString_3 = sectionDescElementsString + ", #menu-sctn_3";
+				fadeSectionBlocks(sectionDescELementsString_3, transitionValue);
+			break;
+			case 4:
+				fadeSectionBlocks(sectionDescElementsString, transitionValue);
+			break;
+		}
 	}	else	{
 		var logoCSS_1 = {
 			display: "block"
@@ -50,11 +68,22 @@ $(document).ready(
 			function () {
 				var browserPositionNum = $(window).scrollTop();
 				
-				if (browserPositionNum > 600) {
-					if (browserPositionNum > 601 || browserPositionNum < 1400) {
-						animateElements(1);
-					}
+				if (browserPositionNum > 500 && browserPositionNum < 800) {
+					animateElements(1);
+				} 
+				
+				if (browserPositionNum > 1780 && browserPositionNum < 2180) {
+					animateElements(2);
+				}	
+				
+				if (browserPositionNum > 3060 && browserPositionNum < 3360)	{
+					animateElements(3);
+				}	
+				
+				if (browserPositionNum > 4140 && browserPositionNum < 4540)	{
+					animateElements(4);
 				}
+						
 				// window.alert("$(window).scrollTop() = " + $(window).scrollTop());
 			});
 	}
